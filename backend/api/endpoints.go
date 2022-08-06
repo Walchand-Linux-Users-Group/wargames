fbb190b51fcd68db3443667dd2000f1acb87ef9d
Suyash Chavan <suyash@gyde.ai>
2022-08-06 21:15:00 +0530

    API Register Completed

--- backend/api/endpoints.go
+++ backend/api/endpoints.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func initAPI() {
-	fmt.Println("Hello")
+
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", alive)
@@ -35,64 +37,68 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
 	type body struct {
-		username string
-		password string
-		apiToken string
+		Username string `json:"username"`
+		Password string `json:"password"`
+		ApiToken string `json:"apiToken"`
 	}
 
-	decoder := json.NewDecoder(r.Body)
-	var user body
+	user := body{}
 
-	err := decoder.Decode(&user)
-
-	fmt.Println(r.Body)
+	err := json.Unmarshal(reqBody, &user)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if user.apiToken != getEnv("API_TOKEN") {
+	if user.ApiToken != getEnv("API_TOKEN") {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	match, err := regexp.MatchString("[A-Za-z0-9]", user.username)
+	match, err := regexp.MatchString("[A-Za-z0-9]", user.Username)
 
-	if !match || len(user.username) > 10 || err != nil {
+	if !match || len(user.Username) > 10 || err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	match, err = regexp.MatchString("[a-zA-Z0-9]", user.password)
+	match, err = regexp.MatchString("[a-zA-Z0-9]", user.Password)
 
-	if !match || len(user.password) > 10 || err != nil {
+	if !match || len(user.Password) > 10 || err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	wargames_db := clientInstance.Database("wargames")
-	users_collection := wargames_db.Collection("users")
+	users_collection := clientInstance.Database("wargames").Collection("users")
 
-	type userdoc struct {
-		username string
-		password string
-		level    int64
+	var dublicate body
+	users_collection.FindOne(ctx, bson.M{
+		"username": user.Username,
+	}).Decode(&dublicate)
+
+	if (dublicate != body{}) {
+		http.Error(w, "User already registered!", http.StatusBadRequest)
+		return
 	}
 
-	_, err = users_collection.InsertOne(ctx, userdoc{
-		username: user.username,
-		password: user.password,
-		level:    0,
+	_, err = users_collection.InsertOne(ctx, bson.M{
+		"username": user.Username,
+		"password": user.Password,
+		"level":    0,
 	})
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	w.WriteHeader(200)
 }
 
 func auth(w http.ResponseWriter, r *http.Request) {
